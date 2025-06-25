2ac4bce10885c3ffe786999d64158200aacffce1
CallMeJimmothy <139634991+CallMeJimmothy@users.noreply.github.com>
2025-06-25 12:27:16 +0100

    Update clicker.py
    inconsistencies in segmentation

--- clicker.py
+++ clicker.py
@@ -205,7 +205,7 @@ upgrade2 = Upgrade("Upgrade 2", 100, 5, "Increases score gain by 5")
 upgrade3 = Upgrade("Upgrade 3", 1000, 50, "Increases score gain by 50")
 
 #---------------------------------------------------------
-#classes
+#clicker
 #---------------------------------------------------------
 
 class Clicker:

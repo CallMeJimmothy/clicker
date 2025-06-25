99c888bbda3df2739944c79d78d95fbd3cf46608
Impermadial <Irmantasmal+modrinth@gmail.com>
2025-06-25 09:54:16 +0100

    add parameters to draw_clicker

--- clicker.py
+++ clicker.py
@@ -212,7 +212,8 @@ class Clicker:
     def __init__(self,x ,y ,width, height):
         self.rect = pygame.Rect(x, y, width, height)
     
-    def draw_clicker(self)
+    def draw_clicker(self, x, y, mouse_pos, mouse_pressed):
+        
 
 #---------------------------------------------------------
 #draw modules

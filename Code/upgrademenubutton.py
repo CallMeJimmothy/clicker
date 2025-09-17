eca20b897c664c195f2a83e33655fa84f54c4920
Impermadial <irmantasmal@gmail.com>
2025-09-17 21:38:04 +0100

    changes

--- Code/upgrademenubutton.py
+++ Code/upgrademenubutton.py
@@ -2,33 +2,26 @@ import pygame
 from engine import engine
 from gamestate import game_state
 
-""" def upgmenu_switcher_btn(game_state):
-        upgrademenu_selection = ("manual", "auto")
-        upgrademenuselectionpointer = upgrademenu_selection[1]
-
-        if game_state.upgradesmenu == "manual":
-            upgrademenuselectionpointer = upgrademenu_selection[1]
-        
-        if game_state.upgradesmenu == "auto":
-              upgrademenuselectionpointer = upgrademenu_selection[0]
-
-        upgrademenuswitcher_text = engine.font.render(f"change menu to {upgrademenuselectionpointer}", True, "White") """
-
-
 class upgrademenu_switcher_btn:
       
-      ANIMATION_DURATION = 20
-      HOVER_ANIMATION_SPEED = 0.1
+    ANIMATION_DURATION = 20
+    HOVER_ANIMATION_SPEED = 0.1
 
-      def __init__(self, x, y, sizeX, sizeY):
+    def __init__(self, x, y, sizeX, sizeY):
         self.x = x
         self.y = y
         self.sizeX = sizeX
         self.sizeY = sizeY
 
-      def draw_button(self, mouse_pos, mouse_pressed, clicked):
-          default_colour = "white"
-          text_colour = "black"
+    def draw_button(self, mouse_pos, mouse_pressed, clicked):
+        default_colour = "white"
+        text_colour = "black"
+
+        if game_state.upgradesmenu == "Manual upgrades":
+            upgrademenu_text = "Manual"
+        if game_state.upgradesmenu == "Automatic upgrades":
+            upgrademenu_text = "Automatic"
+              
 
 
 upgrademenu_switcher_button = upgrademenu_switcher_btn(20,100, 50, 100)
